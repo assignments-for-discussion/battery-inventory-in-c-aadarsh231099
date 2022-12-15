d871ab98224b30b89b4ac66a0106e1552b362a88
Aadarsh <4nm19is001@nmamit.in>
2022-12-15 17:37:05 +0530

    added additional test case for boundary checking

--- bucketer.c
+++ bucketer.c
@@ -15,13 +15,13 @@ struct CountsByUsage countBatteriesByUsage(const int* cycles, int nBatteries) {
   counts.highCount=0;
   for(int i=0;i<nBatteries;i++){
       
-  if(cycles[i]>0 && cycles[i]<310){
+  if(cycles[i]>=0 && cycles[i]<310){
       counts.lowCount++;
   }
-  else if(cycles[i]>=310 && cycles[i]<=929){
+  else if(cycles[i]>=310 && cycles[i]<930){
       counts.mediumCount++;
   }
-  else{
+  else if(cycles[i]>=930){
       counts.highCount++;
   }
  }
@@ -36,10 +36,28 @@ void testBucketingByNumberOfCycles() {
   assert(counts.lowCount == 2);
   assert(counts.mediumCount == 3);
   assert(counts.highCount == 1);
+  printf("Low count is: %d\n",counts.lowCount);
+  printf("Med count is: %d\n",counts.mediumCount);
+  printf("High count is: %d\n",counts.highCount);
+  printf("Done counting :)\n");
+}
+
+void testBucketingByNumberOfCycles1() {
+  const int chargeCycleCounts[] = {0, -1, 929, 310, 900, 1000};
+  const int numberOfBatteries = sizeof(chargeCycleCounts) / sizeof(chargeCycleCounts[0]);
+  printf("Counting batteries by usage cycles...\n");
+  struct CountsByUsage counts = countBatteriesByUsage(chargeCycleCounts, numberOfBatteries);
+  assert(counts.lowCount == 1);
+  assert(counts.mediumCount == 3);
+  assert(counts.highCount == 1);
+  printf("Low count is: %d\n",counts.lowCount);
+  printf("Med count is: %d\n",counts.mediumCount);
+  printf("High count is: %d\n",counts.highCount);
   printf("Done counting :)\n");
 }
 
 int main() {
   testBucketingByNumberOfCycles();
+  testBucketingByNumberOfCycles1();
   return 0;
 }
